3c13daec29eee27fd89a466b9595de8189c68c4a
Anton Popoff <panton295@gmail.com>
2024-04-12 01:09:34 +0900

    Load colorscheme with the plugin definition

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 return {
     'rebelot/kanagawa.nvim',
-
-    opts = {
-        theme = "dragon",    -- Set "Dragon" palette as a default for Kanagwa theme
-        background = "none", -- Disable background so "theme" attributes works
-        compile = true,
-        -- Disable background color for line numbers column
-        colors = { theme = { all = { ui = { bg_gutter = "none" } } } }
-    },
+    config = function()
+        require('kanagawa').setup({
+            theme = 'dragon',    -- Set 'Dragon' palette as a default for Kanagwa theme
+            background = 'none', -- Disable background so 'theme' attributes works
+            compile = true,
+            -- Disable background color for line numbers column
+            colors = { theme = { all = { ui = { bg_gutter = 'none' } } } }
+        })
+        vim.cmd('colorscheme kanagawa')
+    end,
 }
-
