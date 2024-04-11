e7d2eef281774f38723b7df1710809c9af72a9bd
Anton Popoff <panton295@gmail.com>
2024-04-12 01:11:23 +0900

    Remove colorscheme loading from init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,7 @@
 -- Neovim config start!
-
 vim.g.mapleader = ' '
-
 require("lazybootstrap")
 require("lsp_config")
-require("kanagawa").load()
 require('dap_codelldb')
 require("vim_mappings")
 require('vim_config')
