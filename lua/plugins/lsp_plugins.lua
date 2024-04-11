350648255fb9b3c84f6289af323d9abed65802f7
Anton Popoff <panton295@gmail.com>
2024-04-12 01:10:42 +0900

    Clean up LSP plugins definition

--- lua/plugins/lsp_plugins.lua
+++ lua/plugins/lsp_plugins.lua
@@ -1,16 +1,12 @@
 return {
-    -- "LSP Zero" Plugins
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x'
     },
-    { 'neovim/nvim-lspconfig', },
-
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'L3MON4D3/LuaSnip' },
-
-    -- Mason plugin to mange LSP configs plugin-like way
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
 }
