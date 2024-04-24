fa126c7ba1f4b203b46c99bab9c1a7d9f1da09da
Anton Popoff <panton295@gmail.com>
2024-04-25 00:12:08 +0900

    Tweak Kanagawa theme just a little bit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,30 @@
 return {
-    'rebelot/kanagawa.nvim',
-    config = function()
-        require('kanagawa').setup({
-            theme = 'dragon',    -- Set 'Dragon' palette as a default for Kanagwa theme
-            background = 'none', -- Disable background so 'theme' attributes works
-            compile = true,
-            -- Disable background color for line numbers column
-            colors = { theme = { all = { ui = { bg_gutter = 'none' } } } }
-        })
-        vim.cmd('colorscheme kanagawa')
-    end,
+    {
+        'rebelot/kanagawa.nvim',
+        lazy = false,
+        config = function()
+            require('kanagawa').setup({
+                theme = 'dragon',
+                background = 'none',
+                compile = true,
+                colors = { theme = { all = { ui = { bg_gutter = 'none', bg_p2 = "none" } } } },
+                transparent = true,
+                overrides = function(colors)
+                    local theme = colors.theme
+                    return {
+                        EndOfBuffer = { fg = theme.ui.nontext, bg = theme.ui.bg_gutter },
+                        NonText = { fg = theme.ui.nontext, bg = theme.ui.bg_gutter },
+                        TelescopeTitle = { fg = theme.ui.special, bold = true },
+                        TelescopeResultsBorder = { fg = theme.ui.nontext, bg = "none" },
+                        TelescopePreviewBorder = { fg = theme.ui.nontext, bg = "none" },
+                        TelescopePromptBorder = { fg = theme.ui.nontext, bg = "none" },
+                        TelescopePromptNormal = { bg = "none" },
+                        TelescopeResultsNormal = { bg = "none" },
+                        TelescopePreviewNormal = { bg = "none" },
+                    }
+                end
+            })
+            vim.cmd('colorscheme kanagawa')
+        end,
+    },
 }
